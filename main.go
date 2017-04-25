2be151117d13875815d2796023b5d404fc61e795
Benjamin <ben-st@diffus.org>
2017-04-25 21:44:56 +0200

    initial client

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+const url = "https://api.github.com/users/ben-st"
+
+// GithubUser define the github api
+type GithubUser struct {
+	Login             string      `json:"login"`
+	ID                int         `json:"id"`
+	AvatarURL         string      `json:"avatar_url"`
+	GravatarID        string      `json:"gravatar_id"`
+	URL               string      `json:"url"`
+	HTMLURL           string      `json:"html_url"`
+	FollowersURL      string      `json:"followers_url"`
+	FollowingURL      string      `json:"following_url"`
+	GistsURL          string      `json:"gists_url"`
+	StarredURL        string      `json:"starred_url"`
+	SubscriptionsURL  string      `json:"subscriptions_url"`
+	OrganizationsURL  string      `json:"organizations_url"`
+	ReposURL          string      `json:"repos_url"`
+	EventsURL         string      `json:"events_url"`
+	ReceivedEventsURL string      `json:"received_events_url"`
+	Type              string      `json:"type"`
+	SiteAdmin         bool        `json:"site_admin"`
+	Name              string      `json:"name"`
+	Company           interface{} `json:"company"`
+	Blog              interface{} `json:"blog"`
+	Location          interface{} `json:"location"`
+	Email             interface{} `json:"email"`
+	Hireable          interface{} `json:"hireable"`
+	Bio               interface{} `json:"bio"`
+	PublicRepos       int         `json:"public_repos"`
+	PublicGists       int         `json:"public_gists"`
+	Followers         int         `json:"followers"`
+	Following         int         `json:"following"`
+	CreatedAt         time.Time   `json:"created_at"`
+	UpdatedAt         time.Time   `json:"updated_at"`
+}
+
+func main() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	v := GithubUser{}
+	err = decoder.Decode(&v)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(v.Name, v.PublicRepos, v.Blog)
+}
